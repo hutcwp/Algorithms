08208047040903df632c03d288f154eb7fa3193b
caiwenpeng <caiwenpeng@yy.com>
2019-12-27 20:46:08 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * for java code
+ * created by hutcwp
+ * 2019.12.27
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world!");
+    }
+}
